076b5db342025e9b47dc4188a2b7975e84f42cad
Neil Roberts <neil@linux.intel.com>
2014-06-14 22:05:28 +0100

    Draw a triangle

--- glthing.c
+++ glthing.c
@@ -2,19 +2,262 @@
 #include <SDL.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <stdarg.h>
 
 struct glthing {
         SDL_Window *window;
         SDL_GLContext gl_context;
+        struct triangle *triangle;
+        GLuint program;
 
         bool quit;
 };
+
+struct vertex {
+        float x, y;
+};
+
+struct triangle {
+        GLuint vertex_array;
+        GLuint buffer;
+};
+
+#define ATTRIB_POS 0
+
+static char *
+load_shader_source(const char *filename)
+{
+        FILE *file;
+        char *source;
+        long int size;
+        int res;
+
+        file = fopen(filename, "r");
+        if (file == NULL) {
+                fprintf(stderr, "%s: %s\n", filename, strerror(errno));
+                return NULL;
+        }
+
+        res = fseek(file, 0L, SEEK_END);
+        if (res) {
+                fprintf(stderr, "%s: %s\n", filename, strerror(errno));
+                fclose(file);
+                return NULL;
+        }
+
+        size = ftell(file);
+        if (size == -1) {
+                fprintf(stderr, "%s: %s\n", filename, strerror(errno));
+                fclose(file);
+                return NULL;
+        }
+
+        res = fseek(file, 0L, SEEK_SET);
+        if (res) {
+                fprintf(stderr, "%s: %s\n", filename, strerror(errno));
+                fclose(file);
+                return NULL;
+        }
+
+        source = malloc(size + 1);
+        source[size] = '\0';
+
+        res = fread(source, 1, size, file);
+        if (res != size) {
+                if (ferror(file))
+                        fprintf(stderr, "%s: %s\n", filename, strerror(errno));
+                else
+                        fprintf(stderr, "%s: unexpected EOF\n", filename);
+                fclose(file);
+                free(source);
+                return NULL;
+        }
+
+        fclose(file);
+
+        return source;
+}
+
+static GLuint
+load_shader(GLenum type,
+            const char *filename)
+{
+        GLuint shader;
+        GLint length, compile_status;
+        GLsizei actual_length;
+        char *source;
+        GLchar *info_log;
+
+        source = load_shader_source(filename);
+        if (source == NULL)
+                return 0;
+
+        length = strlen(source);
+
+        shader = glCreateShader(type);
+        glShaderSource(shader, 1, (const GLchar **) &source, &length);
+
+        free(source);
+
+        glCompileShader(shader);
+
+        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &length);
+
+        if (length > 0) {
+                info_log = malloc(length);
+                glGetShaderInfoLog(shader, length, &actual_length, info_log);
+                if (*info_log) {
+                        fprintf(stderr,
+                                "Info log for %s:\n%s\n",
+                                filename,
+                                info_log);
+                }
+                free(info_log);
+        }
+
+        glGetShaderiv(shader, GL_COMPILE_STATUS, &compile_status);
+
+        if (!compile_status) {
+                fprintf(stderr, "%s: compilation failed\n", filename);
+                glDeleteShader(shader);
+                return 0;
+        }
+
+        return shader;
+}
+
+static GLuint
+create_program_with_shaders(GLenum shader_type,
+                            ...)
+{
+        GLint length, link_status;
+        GLsizei actual_length;
+        GLchar *info_log;
+        GLuint program;
+        GLuint shader;
+        va_list ap;
+
+        va_start(ap, shader_type);
+
+        program = glCreateProgram();
+
+        while (shader_type != GL_NONE) {
+                shader = load_shader(shader_type, va_arg(ap, const char *));
+                if (shader == 0) {
+                        glDeleteProgram(program);
+                        va_end(ap);
+                        return 0;
+                }
+
+                glAttachShader(program, shader);
+                glDeleteShader(shader);
+
+                shader_type = va_arg(ap, GLenum);
+        }
+
+        va_end(ap);
+
+        glBindAttribLocation(program, ATTRIB_POS, "pos");
+
+        glLinkProgram(program);
+
+        glGetProgramiv(program, GL_INFO_LOG_LENGTH, &length);
+
+        if (length > 0) {
+                info_log = malloc(length);
+                glGetProgramInfoLog(program, length, &actual_length, info_log);
+                if (*info_log) {
+                        fprintf(stderr,
+                                "Link info log:\n%s\n",
+                                info_log);
+                }
+                free(info_log);
+        }
+
+        glGetProgramiv(program, GL_LINK_STATUS, &link_status);
+
+        if (!link_status) {
+                fprintf(stderr, "program link failed\n");
+                glDeleteProgram(program);
+                return 0;
+        }
+
+        return program;
+}
+
+static GLuint
+create_program(void)
+{
+        return create_program_with_shaders(GL_VERTEX_SHADER,
+                                           "vertex-shader.glsl",
+                                           GL_FRAGMENT_SHADER,
+                                           "fragment-shader.glsl",
+                                           GL_NONE);
+}
+
+static struct triangle *
+create_triangle(void)
+{
+        static const struct vertex triangle_data[3] = {
+                { -0.5f, -0.5f },
+                { 0.5f, -0.5f },
+                { 0.0f, 0.5f }
+        };
+        struct triangle *triangle;
+
+        triangle = malloc(sizeof *triangle);
+
+        glGenBuffers(1, &triangle->buffer);
+        glBindBuffer(GL_ARRAY_BUFFER, triangle->buffer);
+        glBufferData(GL_ARRAY_BUFFER,
+                     sizeof triangle_data,
+                     triangle_data,
+                     GL_STATIC_DRAW);
+
+        glGenVertexArrays(1, &triangle->vertex_array);
+        glBindVertexArray(triangle->vertex_array);
+
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(ATTRIB_POS,
+                              2, /* size */
+                              GL_FLOAT,
+                              GL_FALSE, /* normalized */
+                              sizeof (struct vertex),
+                              (void *) offsetof(struct vertex, x));
+
+        glBindVertexArray(0);
+
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+        return triangle;
+}
+
+static void
+free_triangle(struct triangle *triangle)
+{
+        glDeleteVertexArrays(1, &triangle->vertex_array);
+        glDeleteBuffers(1, &triangle->buffer);
+        free(triangle);
+}
 
 static void
 redraw(struct glthing *glthing)
 {
         glClearColor(0.0, 0.0, 0.0, 0.0);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(glthing->program);
+
+        glBindVertexArray(glthing->triangle->vertex_array);
+
+        glDrawArrays(GL_TRIANGLE_STRIP, 0, 3);
+
+        glBindVertexArray(0);
+
+        glUseProgram(0);
 
         SDL_GL_SwapWindow(glthing->window);
 }
@@ -118,9 +361,22 @@ main(int argc, char **argv)
 
         SDL_GL_MakeCurrent(glthing.window, glthing.gl_context);
 
+        glthing.program = create_program();
+        if (glthing.program == 0) {
+                ret = EXIT_FAILURE;
+                goto out_context;
+        }
+
+        glthing.triangle = create_triangle();
+
         if (!main_loop(&glthing))
                 ret = EXIT_FAILURE;
 
+        free_triangle(glthing.triangle);
+
+        glDeleteProgram(glthing.program);
+
+out_context:
         SDL_GL_MakeCurrent(NULL, NULL);
         SDL_GL_DeleteContext(glthing.gl_context);
 out_window:

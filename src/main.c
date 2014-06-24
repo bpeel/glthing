ccf7ec8ef3d1044f6e1f67b2e289dd7aaa8e9da8
Neil Roberts <neil@localhost.localdomain>
2014-06-24 14:53:00 +0100

    Do all three

--- src/main.c
+++ src/main.c
@@ -6,11 +6,29 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <assert.h>
+#include <stdarg.h>
 
 struct glthing {
         SDL_Window *window;
         SDL_GLContext gl_context;
 };
+
+enum blit_mode {
+        BLIT_MODE_BLIT,
+        BLIT_MODE_FIXED_FUNCTION_TRIANGLES,
+        BLIT_MODE_GLSL_TRIANGLES
+};
+
+static const char *
+blit_mode_names[] = {
+        "blit",
+        "ff",
+        "glsl"
+};
+
+#define DST_WIDTH 256
+
+#define ARRAY_SIZE(x) (sizeof (x) / sizeof ((x)[0]))
 
 static void
 create_texture(GLuint *tex,
@@ -29,6 +47,11 @@ create_texture(GLuint *tex,
                      GL_UNSIGNED_BYTE,
                      data);
 
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
         glGenFramebuffers(1, fbo);
         glBindFramebuffer(GL_FRAMEBUFFER, *fbo);
         glFramebufferTexture2D(GL_FRAMEBUFFER,
@@ -42,31 +65,13 @@ create_texture(GLuint *tex,
 }
 
 static void
-do_blit(void)
+blit_fixed_function_triangles(GLuint src_tex, GLuint src_fbo,
+                              GLuint dst_tex, GLuint dst_fbo)
 {
-        GLuint src_tex, dst_tex;
-        GLuint src_fbo, dst_fbo;
-        static const GLubyte src_data[2 * 4] = {
-                0xff, 0x00, 0x00, 0xff,
-                0x00, 0x00, 0xff, 0xff,
-        };
-        GLubyte dst_data[256 * 4];
-        int i, j;
+        glViewport(0, 0, DST_WIDTH, 1);
 
-        create_texture(&src_tex, &src_fbo, 2, 1, src_data);
-        create_texture(&dst_tex, &dst_fbo, 256, 1, NULL);
-
-        glBindFramebuffer(GL_READ_FRAMEBUFFER, src_fbo);
-        glBindFramebuffer(GL_DRAW_FRAMEBUFFER, dst_fbo);
-
-        glViewport(0, 0, 256, 1);
-
-        glEnable(GL_TEXTURE_2D);
         glBindTexture(GL_TEXTURE_2D, src_tex);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glEnable(GL_TEXTURE_2D);
 
         glBegin(GL_TRIANGLE_STRIP);
         glTexCoord2i(0, 0);
@@ -79,6 +84,218 @@ do_blit(void)
         glVertex2i(1, 1);
         glEnd();
 
+        glDisable(GL_TEXTURE_2D);
+}
+
+static GLuint
+create_shader(GLenum type, const char *source)
+{
+        GLuint shader;
+        GLint length, compile_status;
+        GLsizei actual_length;
+        GLchar *info_log;
+
+        length = strlen(source);
+
+        shader = glCreateShader(type);
+        glShaderSource(shader, 1, (const GLchar **) &source, &length);
+
+        glCompileShader(shader);
+
+        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &length);
+
+        if (length > 0) {
+                info_log = malloc(length);
+                glGetShaderInfoLog(shader, length, &actual_length, info_log);
+                if (*info_log) {
+                        fprintf(stderr,
+                                "Info log:\n%s\n",
+                                info_log);
+                }
+                free(info_log);
+        }
+
+        glGetShaderiv(shader, GL_COMPILE_STATUS, &compile_status);
+
+        if (!compile_status) {
+                fprintf(stderr, "compilation failed\n");
+                glDeleteShader(shader);
+                return 0;
+        }
+
+        return shader;
+}
+
+static GLuint
+create_program(GLenum shader_type,
+               ...)
+{
+        GLint length, link_status;
+        GLsizei actual_length;
+        GLchar *info_log;
+        GLuint program;
+        GLuint shader;
+        va_list ap;
+
+        va_start(ap, shader_type);
+
+        program = glCreateProgram();
+
+        while (shader_type != GL_NONE) {
+                shader = create_shader(shader_type,
+                                       va_arg(ap, const char *));
+                if (shader == 0) {
+                        glDeleteProgram(program);
+                        va_end(ap);
+                        return 0;
+                }
+
+                glAttachShader(program, shader);
+                glDeleteShader(shader);
+
+                shader_type = va_arg(ap, GLenum);
+        }
+
+        va_end(ap);
+
+        glLinkProgram(program);
+
+        glGetProgramiv(program, GL_INFO_LOG_LENGTH, &length);
+
+        if (length > 0) {
+                info_log = malloc(length);
+                glGetProgramInfoLog(program, length, &actual_length, info_log);
+                if (*info_log) {
+                        fprintf(stderr,
+                                "Link info log:\n%s\n",
+                                info_log);
+                }
+                free(info_log);
+        }
+
+        glGetProgramiv(program, GL_LINK_STATUS, &link_status);
+
+        if (!link_status) {
+                fprintf(stderr, "program link failed\n");
+                glDeleteProgram(program);
+                return 0;
+        }
+
+        return program;
+}
+
+static void
+blit_glsl_triangles(GLuint src_tex, GLuint src_fbo,
+                    GLuint dst_tex, GLuint dst_fbo)
+{
+        GLuint uniform;
+        GLuint attrib;
+        GLuint prog;
+
+        static const char vertex_shader[] =
+                "varying vec2 coord;\n"
+                "attribute vec2 pos;\n"
+                "\n"
+                "void\n"
+                "main()\n"
+                "{\n"
+                "        coord = pos;\n"
+                "        gl_Position = vec4(pos * 2.0 - 1.0, 0.0, 1.0);\n"
+                "}\n";
+        static const char fragment_shader[] =
+                "varying vec2 coord;\n"
+                "uniform sampler2D tex;\n"
+                "\n"
+                "void\n"
+                "main()\n"
+                "{\n"
+                "        gl_FragColor = texture2D(tex, coord);\n"
+                "}\n";
+
+        static const GLfloat verts[] = {
+                0.0f, 0.0f,
+                1.0f, 0.0f,
+                0.0f, 1.0f,
+                1.0f, 1.0f,
+        };
+
+        prog = create_program(GL_VERTEX_SHADER, vertex_shader,
+                              GL_FRAGMENT_SHADER, fragment_shader,
+                              GL_NONE);
+
+        if (prog == 0)
+                return;
+
+        glBindTexture(GL_TEXTURE_2D, src_tex);
+
+        glUseProgram(prog);
+
+        uniform = glGetUniformLocation(prog, "tex");
+        glUniform1i(uniform, 0);
+
+        attrib = glGetAttribLocation(prog, "pos");
+
+        glEnableVertexAttribArray(attrib);
+        glVertexAttribPointer(attrib,
+                              2, /* size */
+                              GL_FLOAT,
+                              GL_FALSE, /* normalized */
+                              2 * sizeof (float),
+                              verts);
+
+        glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+
+        glDisableVertexAttribArray(attrib);
+
+        glUseProgram(0);
+
+        glDeleteProgram(prog);
+}
+
+static void
+blit_blit(GLuint src_tex, GLuint src_fbo,
+          GLuint dst_tex, GLuint dst_fbo)
+{
+        glBlitFramebuffer(0, 0, /* src x0 y0 */
+                          2, 1, /* src x1 y1 */
+                          0, 0, /* dst x0 y0 */
+                          DST_WIDTH, 1, /* dst x0 y0 */
+                          GL_COLOR_BUFFER_BIT,
+                          GL_LINEAR);
+
+}
+
+static void
+do_blit(enum blit_mode mode, GLubyte *dst_data)
+{
+        GLuint src_tex, dst_tex;
+        GLuint src_fbo, dst_fbo;
+        static const GLubyte src_data[2 * 4] = {
+                0xff, 0x00, 0x00, 0xff,
+                0x00, 0x00, 0xff, 0xff,
+        };
+
+        create_texture(&src_tex, &src_fbo, 2, 1, src_data);
+        create_texture(&dst_tex, &dst_fbo, DST_WIDTH, 1, NULL);
+
+        glBindFramebuffer(GL_READ_FRAMEBUFFER, src_fbo);
+        glBindFramebuffer(GL_DRAW_FRAMEBUFFER, dst_fbo);
+
+        switch (mode) {
+        case BLIT_MODE_BLIT:
+                blit_blit(src_tex, src_fbo,
+                          dst_tex, dst_fbo);
+                break;
+        case BLIT_MODE_FIXED_FUNCTION_TRIANGLES:
+                blit_fixed_function_triangles(src_tex, src_fbo,
+                                              dst_tex, dst_fbo);
+                break;
+        case BLIT_MODE_GLSL_TRIANGLES:
+                blit_glsl_triangles(src_tex, src_fbo,
+                                    dst_tex, dst_fbo);
+                break;
+        }
+
         glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
         glDeleteFramebuffers(1, &src_fbo);
@@ -90,11 +307,29 @@ do_blit(void)
 
         glDeleteTextures(1, &src_tex);
         glDeleteTextures(1, &dst_tex);
+}
 
-        for (i = 0; i < 256; i++) {
-                printf("%02x:  ", i);
-                for (j = 0; j < 4; j++)
-                        printf("%02x", dst_data[i * 4 + j]);
+static void
+do_all_blits(void)
+{
+        GLubyte dst_data[ARRAY_SIZE(blit_mode_names)][DST_WIDTH * 4];
+        int mode;
+        int x, c;
+
+        fputs("  ,  ", stdout);
+        for (mode = 0; mode < ARRAY_SIZE(blit_mode_names); mode++) {
+                do_blit(mode, dst_data[mode]);
+                printf("%-8s,", blit_mode_names[mode]);
+        }
+        fputc('\n', stdout);
+
+        for (x = 0; x < DST_WIDTH; x++) {
+                printf("%02x,  ", x);
+                for (mode = 0; mode < ARRAY_SIZE(blit_mode_names); mode++) {
+                        for (c = 0; c < 4; c++)
+                                printf("%02x", dst_data[mode][x * 4 + c]);
+                        fputc(',', stdout);
+                }
                 fputc('\n', stdout);
         }
 }
@@ -143,7 +378,7 @@ main(int argc, char **argv)
 
         SDL_GL_MakeCurrent(glthing.window, glthing.gl_context);
 
-        do_blit();
+        do_all_blits();
 
         SDL_GL_MakeCurrent(NULL, NULL);
         SDL_GL_DeleteContext(glthing.gl_context);
